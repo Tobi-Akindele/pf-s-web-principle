ad8433f33dfa0b0b30c127cb012472e927ed67a8
Oyindamola Akindele <oyindamolaakindele@Oyindamolas-MacBook-Pro.local>
2022-01-15 20:18:41 +0000

    Hangman done

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -51,16 +51,96 @@ const data = [
   },
 ];
 
+//Game variables
 let idiom = '';
+let questionId = 0;
 let wrongGuessLimit = 6;
 let wrongGuess = 0;
 let guessed = [];
 let idiomStatus = '';
+let highScores = [];
+
+//Timer variables;
+let hour = 0;
+let minute = 0;
+let seconds = 0;
+let totalSeconds = 0;
+let intervalId = null;
+
+function startTimer(interval) {
+  intervalId = setInterval(timer, interval);
+  function timer() {
+    ++totalSeconds;
+    hour = Math.floor(totalSeconds / 3600);
+    minute = Math.floor((totalSeconds - hour * 3600) / 60);
+    seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+    document.getElementById('hour').innerHTML = formatToTwoDigits(hour);
+    document.getElementById('minute').innerHTML = formatToTwoDigits(minute);
+    document.getElementById('seconds').innerHTML = formatToTwoDigits(seconds);
+  }
+}
+
+function stopTimer() {
+  clearInterval(intervalId);
+}
+
+function formatToTwoDigits(number) {
+  return ('0' + number).slice(-2);
+}
+
+function loadHighscores() {
+  highScores = JSON.parse(
+    localStorage.getItem(questionId + '_highscores') || '[]'
+  );
+  showHighScores(highScores);
+}
+
+function showHighScores(scores) {
+  document.getElementById('first').innerHTML = scores
+    ? secToHHMM(scores[0])
+    : '';
+  document.getElementById('second').innerHTML = scores
+    ? secToHHMM(scores[1])
+    : '';
+  document.getElementById('third').innerHTML = scores
+    ? secToHHMM(scores[2])
+    : '';
+}
+
+function secToHHMM(secs) {
+  if (secs) {
+    let hours = Math.floor(secs / 3600);
+    let minutes = Math.floor((secs - hours * 3600) / 60);
+    let seconds = secs - hours * 3600 - minutes * 60;
+    let H = hours <= 0 ? '' : hours === 1 ? hours + ' hour' : hours + ' hours';
+    let M =
+      minutes <= 0
+        ? ''
+        : minutes === 1
+        ? minutes + ' minute'
+        : minutes + ' minutes';
+    let S =
+      seconds <= 0
+        ? ''
+        : seconds === 1
+        ? seconds + ' second'
+        : seconds + ' seconds';
+
+    return H + ' ' + M + ' ' + S;
+  } else {
+    return '';
+  }
+}
 
 function generateQuestion() {
   const question = data[Math.floor(Math.random() * data.length)];
   idiom = question.idiom.toUpperCase();
+  questionId = question.id;
 
+  showHighScores(
+    JSON.parse(localStorage.getItem(question.id + '_highscores') || '[]')
+  );
   document.getElementById('hint').innerHTML = question.hint;
 }
 
@@ -88,12 +168,55 @@ function loadButtons() {
 }
 
 function handleGuess(letter) {
-  console.log(letter);
   guessed.indexOf(letter) === -1 ? guessed.push(letter) : null;
   document.getElementById(letter).setAttribute('disabled', true);
 
   if (idiom.indexOf(letter) >= 0) {
     guessedPhrase();
+    checkGameState();
+  } else if (idiom.indexOf(letter) === -1) {
+    wrongGuess++;
+    updateWrongGuessState();
+    checkGuessLimit();
+  }
+}
+
+function checkGameState() {
+  idiomStatus = idiomStatus.replace(/&nbsp;&nbsp;/g, ' ');
+  idiomStatus = idiomStatus.trim();
+  if (idiom === idiomStatus) {
+    document.getElementById('game-alphabet').innerHTML =
+      'Congratulations you solved this question in ' + secToHHMM(totalSeconds);
+    computeHighscores();
+    showHighScores(highScores);
+    stopTimer();
+  }
+}
+
+function computeHighscores() {
+  let nHighScores = [];
+  highScores = JSON.parse(
+    localStorage.getItem(questionId + '_highscores') || '[]'
+  );
+  highScores.push(totalSeconds);
+  highScores.sort();
+  for (let i = 0; i < 3; i++) {
+    nHighScores[i] = highScores[i];
+  }
+  highScores = nHighScores;
+  localStorage.setItem(questionId + '_highscores', JSON.stringify(highScores));
+}
+
+function updateWrongGuessState() {
+  document.getElementById('hanging').src =
+    '../hangman/graphics/' + wrongGuess + '.png';
+  document.getElementById('wrongGuess').innerHTML = wrongGuess;
+}
+
+function checkGuessLimit() {
+  if (wrongGuess === wrongGuessLimit) {
+    document.getElementById('question').innerHTML = 'Answer: ' + idiom;
+    document.getElementById('game-alphabet').innerHTML = 'Game Over !!!';
   }
 }
 
@@ -109,11 +232,7 @@ function guessedPhrase() {
   idiom
     .split(' ')
     .map((word) => {
-      maskedWords.push(
-        maskWord(word).concat(
-          '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-        )
-      );
+      maskedWords.push(maskWord(word).concat('&nbsp;&nbsp;'));
     })
     .join('');
   idiomStatus = maskedWords.join('');
@@ -123,6 +242,12 @@ function guessedPhrase() {
 document.getElementById('wrongGuess').innerHTML = wrongGuess;
 document.getElementById('wrongGuessLimit').innerHTML = wrongGuessLimit;
 
+function reset() {
+  location.reload();
+}
+
+loadHighscores();
 generateQuestion();
 loadButtons();
 guessedPhrase();
+startTimer(1000);
